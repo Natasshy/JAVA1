c3a6f5e089326a74c910f8b16caa7e588c94d24c
Natasshy <dolmatova-nata@mail.ru>
2020-09-15 17:45:15 +0300

    Ввести с консоли число в бинарном формате. Перевести его в десятичный формат, записать в переменную int и вывести на экран.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,13 @@
+
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        System.out.println("Hi!");
-    }
+        int i = 232;
+//число 232 в двоичной системе
+System.out.println(Integer.toBinaryString(i));
+//обратный перевод из двоичной системы в десятичную
+        System.out.println(Integer.parseInt("11101000", 2));
+        }
 }
+
+
