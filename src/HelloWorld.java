901844715b4331ab813b5ff249f3dcc40ae3f271
Natasshy <dolmatova-nata@mail.ru>
2020-09-16 16:29:51 +0300

    Привела все задачи в правильный вид

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,6 @@
 
-
 public class HelloWorld {
     public static void main(String[] args) {
-        int i = 1011100011;
-//число 1011100011 в бинарной системе
-             System.out.println(Integer.toBinaryString(i));
-//обратный перевод из бинарной системы в десятичную
-                 System.out.println(Integer.parseInt("1011100011", 2));
-        }
+        System.out.println("Hello, world!");
+    }
 }
-
---
+++ src/Task4.java
@@ -0,0 +1,9 @@
+public class Task4 {
+    public static void main(String[] args) {
+        int i = 1011100011;
+//число 1011100011 в бинарной системе
+        System.out.println(Integer.toBinaryString(i));
+//обратный перевод из бинарной системы в десятичную
+        System.out.println(Integer.parseInt("1011100011", 2));
+    }
+}
