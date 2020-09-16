c11f28884907e8fcf4f00ca119f17e243a6e4cc2
Natasshy <dolmatova-nata@mail.ru>
2020-09-16 17:25:01 +0300

    Необходимо вывести на экран таблицу умножения от 1 до 10 для введённого пользователем любого числа.

--- src/TASK5.java
+++ src/TASK5.java
@@ -1,2 +1,10 @@
 public class TASK5 {
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++) {
+            for (int j=0; j<10; j++) {
+                System.out.printf("%5d", (i+1) * (j+1));
+        }
+        System.out.println();
+        }
+    }
 }
