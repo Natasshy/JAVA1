b6ba82cd32a5d81df4021c658d8c729c65b73523
Natasshy <dolmatova-nata@mail.ru>
2020-09-16 16:19:34 +0300

    исправила на бинарную систему

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,12 +2,11 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
-        int i = 232;
-//число 232 в двоичной системе
-System.out.println(Integer.toBinaryString(i));
-//обратный перевод из двоичной системы в десятичную
-        System.out.println(Integer.parseInt("11101000", 2));
+        int i = 1011100011;
+//число 1011100011 в бинарной системе
+             System.out.println(Integer.toBinaryString(i));
+//обратный перевод из бинарной системы в десятичную
+                 System.out.println(Integer.parseInt("1011100011", 2));
         }
 }
-
 
