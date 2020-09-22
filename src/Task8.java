3e32a5533f60be23bd846afd7b642b4931f35eab
Natasshy <dolmatova-nata@mail.ru>
2020-09-22 14:40:29 +0300

    Необходимо с помощью цикла вывести на экран числа от 1 до 50.

---
+++ src/Task8.java
@@ -0,0 +1,11 @@
+public class Task8 {
+    public static void main(String[] args) {
+        int counter = 1;
+        while(counter <= 50){
+            System.out.println(counter);
+            counter++;
+
+        }
+        }
+}
+
