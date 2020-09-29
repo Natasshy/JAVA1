3b00865e770c35962bbf5ec7ae7c22da7704c810
Natasshy <dolmatova-nata@mail.ru>
2020-09-29 16:44:06 +0300

    Необходимо провести сравнение длины двух строк, которые были введены с клавиатуры и записаны в соответствующие переменные. Вывести на экран строку с наибольшей длиной. Если длины равны, вывести соответствующее сообщение.

---
+++ src/Task13.java
@@ -0,0 +1,22 @@
+/*Необходимо провести сравнение длины двух строк,
+которые были введены с клавиатуры и записаны в соответствующие переменные.
+Вывести на экран строку с наибольшей длиной. Если длины равны,
+вывести соответствующее сообщение.
+ */
+import java.util.Scanner;
+public class Task13 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите строку1:");
+        String s = scan.nextLine();
+        System.out.println("Введите строку2:");
+        String d = scan.nextLine();
+
+        if (s.length() > d.length())
+            System.out.println("Самая длинная строка: " + s);
+        if (s.length() < d.length())
+            System.out.println("Самая длинная строка: " + d);
+        if (s.length() == d.length())
+            System.out.print("Строки одинаковы по длине");
+    }
+}
