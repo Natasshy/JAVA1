1e63084f15254063f5d22c9c219f60c712a64123
Natasshy <dolmatova-nata@mail.ru>
2020-09-30 15:17:59 +0300

    Улучшение кода по заданию 10

--- src/Task10.java
+++ src/Task10.java
@@ -7,46 +7,30 @@ import java.util.Scanner;
 public class Task10 {
     // Функция для чтения матрицы
     public static void readMatrixByUser() {
-        int m, n, i, j;
-        Scanner in = null;
-        try {
-            in = new Scanner(System.in);
+
+            Scanner in = new Scanner(System.in);
             System.out.println("Введите количество строк матрицы");
-            m = in.nextInt();
+            int m = in.nextInt();
             System.out.println("Введите количество колонок матрицы");
-            n = in.nextInt();
+            int n = in.nextInt();
 
             // Объявляем матрицу
-            int mx [][] = new int[m][n];
+            int mx1 [][] = new int[m][n];
 
             // Чтение значений матрицы
             System.out.println("Введите элементы(числа) матрицы");
-            for (i = 0; i < m; i++) {
-                for (j = 0; j < n; j++)
-                    mx [i][j] = in.nextInt();
+                for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++)
+                    mx1 [i][j] = in.nextInt();
             }
 
             // Отображение элементов матрицы
-            System.out.println("Введенные значения, умноженные на 3, первая строка");
-            for (i = 0; i < m; i++) {
-                for (j = 0; j <= n; j++)
-
-                    System.out.print(mx[0][j] * 3 + "  ");
-                            }
-        }
-
-        catch (Exception e) {
-        }
-
-        finally {
-            in.close();
-        }
-
+            System.out.println("Введенные значения, умноженные на 3, первая строка");{
+                for (int j = 0; j < n; j++)
+            System.out.print(mx1[0][j] * 3 + "  ");
+            }
     }
     // Код драйвера
-    public static void main(String[] args)
-    {
-        readMatrixByUser();
-    }
+    public static void main(String[] args)   {readMatrixByUser(); }
 
 }
