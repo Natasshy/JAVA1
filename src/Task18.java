fbe09c0ef9d486d42fffc3a025b36aef551d9d9c
Natasshy <dolmatova-nata@mail.ru>
2020-10-14 16:04:43 +0300

    Необходимо написать программу, которая будет выводить на экран текстовые данные  из файла .txt, а потом в этот же файл перезаписывать текстовые данные, введенные  вручную. Количество строк после перезаписи должно быть столько же, сколько и в  изначальном варианте.

--- src/Task18.java
+++ src/Task18.java
@@ -15,48 +15,32 @@ public class Task18 {
         try (BufferedReader br = new BufferedReader(new FileReader("test1.txt"))) {
             while ((s = br.readLine()) != null) {
                 System.out.println(s);
+                lineNumber++;
             }
+            System.out.println("Количество строк в файле: " + lineNumber);
+
+
         } catch (IOException exp) {
             System.out.println("Ошибка ввода-вывода: " + exp);
         }
-        try {
-            File myFile = new File("test1.txt");
-            FileReader fileReader = new FileReader(myFile);
-            LineNumberReader lineNumberReader = new LineNumberReader(fileReader);
-            while (lineNumberReader.readLine() != null) {
-                lineNumber++;
-            }
-
-            System.out.println("Количество строк в файле: " + lineNumber);
-
-            lineNumberReader.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
 
         BufferedReader br = new BufferedReader(
                 new InputStreamReader(System.in));
-        System.out.println("Введите текст для перезаписи предыдующих значений (не более " + lineNumber + " строк).По окончании введите слово 'stop'.");
+        System.out.println("Введите текст для перезаписи предыдующих значений - не более " + lineNumber + " строк");
 
 
             try (FileWriter fw = new FileWriter("test1.txt", false)) {
                 for (int i = 0; i < lineNumber; i++) {
 
                     s = br.readLine();
-                    if (s.compareTo("stop") == 0) break;
                     s = s + "\n";
                     fw.write(s);
                 }
             }
 
-
               catch (IOException exc) {
                 System.out.println("Ошибка ввода-вывода" + exc);
             }
-
-
             System.out.print("Данные успешно перезаписаны в файл \"test1.txt\"");
         }
     }
