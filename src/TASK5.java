8dc4dda2202d6d63f4b66fd93ec78f8099667787
Natasshy <dolmatova-nata@mail.ru>
2020-10-15 16:53:59 +0300

    улучшение кода

--- src/TASK5.java
+++ src/TASK5.java
@@ -1,3 +1,5 @@
+//Необходимо вывести на экран таблицу умножения от 1 до 10
+// для введённого пользователем любого числа.
 public class TASK5 {
     public static void main(String[] args) {
         for (int i=0; i<10; i++) {
--- src/Task4.java
+++ src/Task4.java
@@ -2,8 +2,8 @@ public class Task4 {
     public static void main(String[] args) {
         int i = 1011100011;
 //число 1011100011 в бинарной системе
-        System.out.println(Integer.toBinaryString(i));
+        System.out.println("число 1011100011 в бинарной системе: " + Integer.toBinaryString(i));
 //обратный перевод из бинарной системы в десятичную
-        System.out.println(Integer.parseInt("1011100011", 2));
+        System.out.println("обратный перевод числа 1011100011 из бинарной системы в десятичную: " + Integer.parseInt("1011100011", 2));
     }
 }
